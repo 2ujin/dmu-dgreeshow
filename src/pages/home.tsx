c564b950fd0914191582c68e668369b80aaf7319
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-09-29 14:18:06 +0100

    feat: 동영상 테스트

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,7 @@ import Ex1 from "../assets/ex1.jpg";
 import Ex2 from "../assets/ex2.jpg";
 import Ex3 from "../assets/ex3.jpg";
 import Ex4 from "../assets/ex4.jpg";
+import ReactPlayer from "react-player";
 
 const HomeWrapper = styled.div`
   background-color: #d9d9d9;
@@ -24,6 +25,7 @@ const Wrapper = styled.div`
   width: 100%;
   align-items: center;
   flex-direction: column;
+  margin-bottom: 100px;
 `;
 
 const Home = () => {
@@ -31,7 +33,16 @@ const Home = () => {
     <HomeWrapper>
       <Header />
       {/* <Footer /> */}
+
       <Wrapper>
+        <ReactPlayer
+          playing
+          url={`${process.env.PUBLIC_URL}/video/intro.mp4`}
+          width="100%"
+          height="100%"
+          loop={true}
+          muted
+        />
         <Img src={Ex1} />
         <Img src={Ex2} />
         <Img src={Ex3} />
