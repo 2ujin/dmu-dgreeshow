6e768607d1e6dd12c94a9a7628f13340015792a7
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-10-01 17:03:29 +0100

    feat: intro 페이지 작업중

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -2,46 +2,103 @@ import styled from "styled-components";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import poster from "../assets/poster.png";
+import box from "../assets/box.svg";
 
 const ContentWrapper = styled.div`
-  height: auto;
-  min-height: 100%;
-`;
-
-const Poster = styled.img`
-  width: 400px;
+  height: 2000px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(
+    0deg,
+    #984678 -0.01%,
+    rgba(242, 133, 66, 0.9) 15.61%,
+    #984678 87.81%
+  );
 `;
 
 const Text = styled.div`
-  font-size: 24px;
-  margin-top: auto;
-  margin-left: 25px;
+  color: #eee;
+  font-size: 60px;
+  font-weight: 700;
+  letter-spacing: 7.5px;
+  position: absolute;
+  right: 100px;
+  top: 50px;
+`;
+
+const SubText = styled.div`
+  color: #eee;
+  font-size: 40px;
+  font-weight: 700;
+  letter-spacing: 3.5px;
+  position: absolute;
+  right: 360px;
+  top: 400px;
 `;
 
 const PostWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  padding: 150px 100px 50px;
+  width: 100%;
+  position: relative;
+  margin-top: 100px;
 `;
 
-const DescWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 100px 50px;
-  background: linear-gradient(249deg, #ba6c9e 17.33%, #f7bd96 75.55%);
+const Box = styled.img`
+  position: absolute;
+  top: 0px;
+  right: 5px;
+  width: 1200px;
+  z-index: 0;
+  /* margin-top: 110px; */
 `;
 
-const DescText = styled.div`
-  color: #4d4d4d;
-  font-size: 22px;
-  max-width: 500px;
+const Poster = styled.img`
+  width: 650px;
+  margin-top: 30px;
+  margin-left: 70px;
+  position: relative;
+  z-index: 998;
+`;
+
+const Line1 = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: -30px;
+  width: 450px;
+  height: 2px;
   margin-left: 50px;
+  background-color: #fff;
 `;
 
-const Temp = styled.div`
-  width: 300px;
-  height: 400px;
-  background-color: darkgray;
+const Line2 = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: -30px;
+  height: 450px;
+  width: 2px;
+  margin-left: 50px;
+  background-color: #fff;
+`;
+
+const Line3 = styled.div`
+  position: absolute;
+  bottom: -510px;
+  left: -70px;
+  height: 450px;
+  width: 3px;
+  margin-left: 50px;
+  background-color: #fff;
+`;
+
+const Line4 = styled.div`
+  position: absolute;
+  bottom: -60px;
+  left: -70px;
+  height: 3px;
+  width: 450px;
+  margin-left: 50px;
+  background-color: #fff;
 `;
 
 const Intro = () => {
@@ -50,26 +107,24 @@ const Intro = () => {
       <Header />
       <ContentWrapper>
         <PostWrapper>
-          <Poster src={poster} />
-          <Text>2023 동양미래대학교 졸업전시회</Text>
+          <div className="poster-wrapper">
+            <Poster src={poster} />
+            <Line1 />
+            <Line2 />
+          </div>
+          <div className="box-wrapper">
+            <Box src={box} />
+            <Line3 />
+            <Line4 />
+          </div>
+          <Text>
+            <b>공간</b>에는 <b>힘</b>이 있다
+          </Text>
+          <SubText>空間力 ; 공간력</SubText>
         </PostWrapper>
-        <DescWrapper>
-          <Temp>1</Temp>
-          <DescText>
-            소리다.이것은 바이며, 우리 살 우리 가는 가슴이 사막이다. 능히 새
-            얼음에 밝은 풍부하게 때문이다. 주는 인도하겠다는 못할 불어 것이다.
-            구하지 크고 못할 것은 모래뿐일 끓는 목숨이 아름다우냐? 하였으며,
-            사는가 주는 있는 무한한 위하여서 풍부하게 대고, 굳세게 황금시대다.
-            바이며, 하였으며, 않는 붙잡아 가는 이상은 것이다. 소리다.이것은
-            바이며, 우리 살 우리 가는 가슴이 사막이다. 능히 새 얼음에 밝은
-            풍부하게 때문이다. 주는 인도하겠다는 못할 불어 것이다. 구하지 크고
-            못할 것은 모래뿐일 끓는 목숨이 아름다우냐? 하였으며, 사는가 주는
-            있는 무한한 위하여서 풍부하게 대고, 굳세게 황금시대다
-          </DescText>
-        </DescWrapper>
       </ContentWrapper>
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
