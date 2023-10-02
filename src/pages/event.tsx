4a4a8b8e3ded595a2ce8249d0817e36fb211764a
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-10-02 17:07:27 +0100

    feat: 이벤트 페이지 마무링!

---
+++ src/pages/event.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Header from "../components/header";
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 150px;
+`;
+
+const TextItem = styled.div`
+  display: flex;
+  width: 75%;
+  justify-content: space-between;
+`;
+const Date = styled.div`
+  color: #000;
+  font-size: 86px;
+  font-weight: 900;
+  line-height: normal;
+  flex: 1;
+  transition: 0.2s all;
+`;
+
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+  color: #000;
+  font-size: 44px;
+  font-weight: 800;
+  flex: 1;
+  text-align: left;
+  margin-left: 30px;
+  transition: 0.2s all;
+`;
+
+const Hr = styled.div`
+  width: 75%;
+  height: 3px;
+  background-color: #984678;
+  margin-top: 30px;
+  margin-bottom: 55px;
+`;
+
+const Event = () => {
+  const list = [
+    { date: "10.16 - 10.24", text: "졸업작품 전시회 기간" },
+    { date: "10.18", text: "졸업작품 전시회 오프닝 행사" },
+    { date: "10.20", text: "졸업작품 경진대회" },
+    { date: "10.21", text: "동문회의 밤" },
+  ];
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        {list.map((item, index) => (
+          <>
+            <TextItem className="events" key={index}>
+              <Date className="event-date">{item.date}</Date>
+              <Text className="event-text">{item.text}</Text>
+            </TextItem>
+
+            {index === list.length - 1 ? "" : <Hr />}
+          </>
+        ))}
+      </Wrapper>
+    </>
+  );
+};
+
+export default Event;
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
 import Intro from "./intro";
+import Profile from "./profile";
+import Event from "./event";
 
 const RoutesComponent = () => {
   return (
     <Routes>
-      <Route path="/*" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/intro" element={<Intro />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
