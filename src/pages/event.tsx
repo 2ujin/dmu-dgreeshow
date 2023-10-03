8eb4c7faa64ab9de9719fa91acde0b06f769585f
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-10-03 18:31:25 +0100

    feat: 이벤트 앱 스타일 적용

--- src/pages/event.tsx
+++ src/pages/event.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "react-responsive";
 import styled from "styled-components";
 import Header from "../components/header";
 
@@ -7,12 +8,23 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 150px;
+
+  &.w-mobile {
+    margin-top: 100px;
+  }
 `;
 
 const TextItem = styled.div`
   display: flex;
   width: 75%;
   justify-content: space-between;
+
+  &.w-mobile {
+    padding: 16px;
+    align-items: flex-start;
+    width: 100%;
+    flex-direction: column;
+  }
 `;
 const Date = styled.div`
   color: #000;
@@ -21,19 +33,27 @@ const Date = styled.div`
   line-height: normal;
   flex: 1;
   transition: 0.2s all;
-  // test
+
+  &.w-mobile {
+    font-size: 50px;
+  }
 `;
 
 const Text = styled.div`
   display: flex;
   align-items: center;
   color: #000;
-  font-size: 44px;
+  font-size: 38px;
   font-weight: 800;
   flex: 1;
   text-align: left;
   margin-left: 30px;
   transition: 0.2s all;
+
+  &.w-mobile {
+    margin-left: 0px;
+    font-size: 33px;
+  }
 `;
 
 const Hr = styled.div`
@@ -42,6 +62,12 @@ const Hr = styled.div`
   background-color: #984678;
   margin-top: 30px;
   margin-bottom: 55px;
+
+  &.w-mobile {
+    width: 100%;
+    margin-top: 20px;
+    margin-bottom: 25px;
+  }
 `;
 
 const Event = () => {
@@ -51,18 +77,31 @@ const Event = () => {
     { date: "10.20", text: "졸업작품 경진대회" },
     { date: "10.21", text: "동문회의 밤" },
   ];
+  const isMobile = useMediaQuery({ query: "(max-width:768px)" });
+
   return (
     <>
       <Header />
-      <Wrapper>
+      <Wrapper className={isMobile ? "w-mobile" : ""}>
         {list.map((item, index) => (
           <>
-            <TextItem className="events" key={index}>
-              <Date className="event-date">{item.date}</Date>
-              <Text className="event-text">{item.text}</Text>
+            <TextItem
+              className={`events ${isMobile ? "w-mobile" : ""}`}
+              key={index}
+            >
+              <Date className={`event-date ${isMobile ? "w-mobile" : ""}`}>
+                {item.date}
+              </Date>
+              <Text className={`event-text ${isMobile ? "w-mobile" : ""}`}>
+                {item.text}
+              </Text>
             </TextItem>
 
-            {index === list.length - 1 ? "" : <Hr />}
+            {index === list.length - 1 ? (
+              ""
+            ) : (
+              <Hr className={isMobile ? "w-mobile" : ""} />
+            )}
           </>
         ))}
       </Wrapper>
