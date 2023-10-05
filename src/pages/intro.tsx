bc2fa26968469b02a5ad380a6ec5da6592d47816
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-10-05 17:59:50 +0100

    feat: 인트로 모바일 화면 구현

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -6,10 +6,12 @@ import box from "../assets/box.svg";
 import box2 from "../assets/box2.svg";
 import useScrollFadeIn from "../hooks/useScrollFadeIn";
 import map from "../assets/map.png";
+import mintro from "../assets/m-intro.png";
+import mbg from "../assets/m-bg.svg";
 import { useState, useEffect, useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const ContentWrapper = styled.div`
-  height: 4000px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -19,6 +21,7 @@ const ContentWrapper = styled.div`
     rgba(242, 133, 66, 0.9) 15.61%,
     #984678 87.81%
   );
+  height: 4000px;
 `;
 
 const Text = styled.div`
@@ -142,6 +145,21 @@ const Desc = styled.div`
   }
 `;
 
+const MDesc = styled.div`
+  color: #fff;
+  text-align: justify;
+  font-size: 13px;
+  font-weight: 500;
+  line-height: 251.8%; /* 60.432px */
+  letter-spacing: 1.32px;
+  padding: 0 17px;
+  margin-top: 10px;
+
+  &.frame-in {
+    animation: ${frameInAnimation} 0.8s forwards;
+  }
+`;
+
 const Box2Wrapper = styled.div`
   position: absolute;
   top: 1500px;
@@ -168,6 +186,14 @@ const Opening = styled.div`
   &.fade-in2 {
     animation: ${frameInAnimation} 1s forwards;
   }
+
+  &.m-intro {
+    position: relative;
+    top: 0;
+    left: 0;
+    font-size: 20px;
+    margin-bottom: 14px;
+  }
 `;
 
 const Date = styled.div`
@@ -180,6 +206,13 @@ const Date = styled.div`
   left: 160px;
   &.fade-in2 {
     animation: ${frameInAnimation} 1s forwards;
+  }
+  &.m-intro {
+    position: relative;
+    top: 0;
+    left: 0;
+    font-size: 36px;
+    margin-bottom: 14px;
   }
 `;
 
@@ -198,6 +231,12 @@ const Where = styled.div`
   &.fade-in2 {
     animation: ${frameInAnimation} 1s forwards;
   }
+  &.m-intro {
+    position: relative;
+    top: 0;
+    left: 0;
+    font-size: 40px;
+  }
 `;
 
 const Map = styled.img`
@@ -206,10 +245,43 @@ const Map = styled.img`
   top: 280px;
   left: 160px;
   z-index: 9;
+
+  &.m-intro {
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 380px;
+  }
+`;
+
+const MIntro = styled.img`
+  width: 80%;
+  margin-top: 20px;
+`;
+
+const MPosterWrapper = styled.div`
+  background-image: url(${mbg});
+  width: 100%;
+  height: 500px;
+  zoom: 0.7;
+  background-repeat: no-repeat;
+  margin-top: 20px;
+  position: relative;
+`;
+
+const MPoster = styled.img`
+  width: 350px;
+  position: absolute;
+  top: 35px;
+  left: 40px;
+  z-index: 998;
 `;
 
 const Intro = () => {
   const animatedItem: any = useScrollFadeIn("up", 1, 0.1);
+  const animatedItem1: any = useScrollFadeIn("down", 1, 0.1);
+  const animatedItem2: any = useScrollFadeIn("right", 1, 0.5);
+
   const [isInViewport, setIsInViewport] = useState(false);
   const ref = useRef<HTMLDivElement | null>(null);
 
@@ -236,58 +308,106 @@ const Intro = () => {
     };
   }, []);
 
+  const isMobile = useMediaQuery({ query: "(max-width:768px)" });
+
   return (
     <>
-      <Header />
-      <ContentWrapper>
-        <PostWrapper>
-          <div className="poster-wrapper">
-            <Poster src={poster} />
-            <Line1 />
-            <Line2 />
-          </div>
-          <div className="box-wrapper-2">
-            <Desc className={isInViewport ? "frame-in" : ""} ref={ref}>
+      {!isMobile ? (
+        <>
+          <Header />
+          <ContentWrapper>
+            <PostWrapper>
+              <div className="poster-wrapper">
+                <Poster src={poster} />
+                <Line1 />
+                <Line2 />
+              </div>
+              <div className="box-wrapper-2">
+                <Desc className={isInViewport ? "frame-in" : ""} ref={ref}>
+                  코로나 팬데믹 이후, 소비 패턴이 물질적인 제품에서 경험 중심의
+                  소비로 크게 변화했다. 이러한 변화는 상업 공간 또한 공간의
+                  크기와 디자인에 영향을 미쳤으며, 무인 서비스 도입과 같은
+                  혁신적인 변화가 이뤄졌다. 이제는 어떻게 사람들에게 풍부한
+                  경험을 제공하며, 어떻게 사람들을 모으고 머물게 할 것인지가
+                  중요한 고려사항이 되었다. 이러한 힘과 현상을 "공간력"이라
+                  정의한다.
+                  <br />
+                  <br />
+                  동양미래대학교 실내건축디자인학과 제22회 졸업전시는 '공간력'을
+                  주제로 혁신적인 공간 실험을 진행합니다. "비어있는 간 사이"란
+                  용어는 빈 공간이 아닌 학생들의 창의력과 관람객의 기억으로
+                  가득한 공간으로 만들어가는 과정을 나타내고 이번 졸업전시를
+                  통해 엔데믹 시대의 경험과 기억을 함께 나누고자 합니다.
+                </Desc>
+                <Box src={box} />
+                <Line3 />
+                <Line4 />
+              </div>
+              <Text>
+                <b>공간</b>에는 <b>힘이</b>있다
+              </Text>
+              <SubText>空間力 ; 공간력</SubText>
+
+              <Box2Wrapper>
+                <div {...animatedItem}>
+                  <Opening>OPENING 2023.10.18</Opening>
+                  <Date>2023.10.16-10.22</Date>
+                  <Where>
+                    동양미래대학교 8-1F <br />| 6DM GALLERY
+                  </Where>
+                </div>
+                <a href="https://naver.me/GkKrYVUz" target="_blank">
+                  <Map src={map} />
+                </a>
+                <Box2 src={box2} />
+              </Box2Wrapper>
+            </PostWrapper>
+          </ContentWrapper>
+
+          <Footer />
+        </>
+      ) : (
+        <>
+          <Header />
+          <ContentWrapper className="m-height">
+            <MIntro {...animatedItem2} src={mintro}></MIntro>
+            <MDesc ref={ref}>
               코로나 팬데믹 이후, 소비 패턴이 물질적인 제품에서 경험 중심의
               소비로 크게 변화했다. 이러한 변화는 상업 공간 또한 공간의 크기와
               디자인에 영향을 미쳤으며, 무인 서비스 도입과 같은 혁신적인 변화가
               이뤄졌다. 이제는 어떻게 사람들에게 풍부한 경험을 제공하며, 어떻게
               사람들을 모으고 머물게 할 것인지가 중요한 고려사항이 되었다.
               이러한 힘과 현상을 "공간력"이라 정의한다.
-              <br />
-              <br />
+            </MDesc>
+            <MPosterWrapper>
+              <MPoster src={poster} />
+            </MPosterWrapper>
+            <MDesc className={isInViewport ? "frame-in" : ""} ref={ref}>
               동양미래대학교 실내건축디자인학과 제22회 졸업전시는 '공간력'을
               주제로 혁신적인 공간 실험을 진행합니다. "비어있는 간 사이"란
               용어는 빈 공간이 아닌 학생들의 창의력과 관람객의 기억으로 가득한
               공간으로 만들어가는 과정을 나타내고 이번 졸업전시를 통해 엔데믹
               시대의 경험과 기억을 함께 나누고자 합니다.
-            </Desc>
-            <Box src={box} />
-            <Line3 />
-            <Line4 />
-          </div>
-          <Text>
-            <b>공간</b>에는 <b>힘이</b>있다
-          </Text>
-          <SubText>空間力 ; 공간력</SubText>
+            </MDesc>
 
-          <Box2Wrapper>
-            <div {...animatedItem}>
-              <Opening>OPENING 2023.10.18</Opening>
-              <Date>2023.10.16-10.22</Date>
-              <Where>
+            <div className="m-text-wrapper" {...animatedItem}>
+              <Opening className={isMobile ? "m-intro" : ""}>
+                OPENING 2023.10.18
+              </Opening>
+              <Date className={isMobile ? "m-intro" : ""}>
+                2023.10.16-10.22
+              </Date>
+              <Where className={isMobile ? "m-intro" : ""}>
                 동양미래대학교 8-1F <br />| 6DM GALLERY
               </Where>
             </div>
-            <a href="https://naver.me/GkKrYVUz" target="_blank">
-              <Map src={map} />
-            </a>
-            <Box2 src={box2} />
-          </Box2Wrapper>
-        </PostWrapper>
-      </ContentWrapper>
 
-      <Footer />
+            <a href="https://naver.me/GkKrYVUz" target="_blank">
+              <Map src={map} className={isMobile ? "m-intro" : ""} />
+            </a>
+          </ContentWrapper>
+        </>
+      )}
     </>
   );
 };
